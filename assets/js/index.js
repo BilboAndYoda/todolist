ee6f719285cb5188778d4b8a4a87d0a694031209
bilboandyoda <jordboudreau@gmail.com>
2017-12-20 22:32:08 -0800

    add js functionality to list

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,20 @@
-var items = $("li");
-
+var newTodo = $("#newTodo");
 //check off specific todos by clicking
-$(items).on("click", function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("cross");
+});
+//remove item from list if clicked on
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+//add new item on enter
+$(newTodo).on("keypress", function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span>"+todoText+"</li>")
+	}
 });
