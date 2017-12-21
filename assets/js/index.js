f127625e50f73ec6413efbdf2f2da77e30e5d627
bilboandyoda <jordboudreau@gmail.com>
2017-12-20 23:28:54 -0800

    add styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ $("ul").on("click", "li", function() {
 });
 //remove item from list if clicked on
 $("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(500, function(){
+	$(this).parent().fadeOut(300, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
@@ -15,6 +15,10 @@ $(newTodo).on("keypress", function(event){
 	if(event.which === 13){
 		var todoText = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span>X</span>"+todoText+"</li>")
+		$("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span>"+todoText+"</li>")
 	}
+});
+
+$(".fa-plus-square").on("click", function(){
+	$("input").toggle(20);
 });
